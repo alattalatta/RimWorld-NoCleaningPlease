63e9a17482f1b2f4585b15897c00b1fdf7281865
urty5656 <urty5656@gmail.com>
2016-07-21 12:07:56 +0900

    Backward compatibility with 1.0.0

---
+++ RimWorld-NoCleaningPlease/Util/BackwardCompat.cs
@@ -0,0 +1,36 @@
+﻿using Verse;
+
+namespace LT
+{
+    // TODO: Get rid of these
+    // Here to not break any saves
+
+    public class MapComponent_NCP : MapComponent
+    {
+        bool done = false;
+        public override void MapComponentUpdate()
+        {
+            if (done)
+            {
+                return;
+            }
+
+            var allFilth = Verse.Find.ListerThings.ThingsInGroup(ThingRequestGroup.Filth);
+
+            for (var i = 0; i < allFilth.Count; i++)
+            {
+                if (allFilth[i].GetType() == typeof(RimWorld.Filth))
+                {
+                    continue;
+                }
+                allFilth[i] = allFilth[i] as RimWorld.Filth;
+            }
+
+            done = true;
+        }
+    }
+
+    public class Filth : RimWorld.Filth
+    {
+    }
+}
